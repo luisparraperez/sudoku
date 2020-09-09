2753f8911e6a60ae57e2e2191d38e8ec29173bf0
luisparraperez <luisparraperez1@gmail.com>
2020-09-09 16:32:38 -0300

    se termina resolucion de sudoku por consola

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,15 @@
 class sudoku:
     def __init__(self,array):
         self.array=array
+        self.N=9
+    def imprimeMatriz(self):
+    
+        for i in  self.array:
+            fila="|"
+            for j in i:
+                fila+= str(j)+'|'
+            print fila
+                
     def validaFila(self,i,valor):
         for j in range(9):
             if self.array[i][j] == valor:
@@ -26,9 +35,46 @@ class sudoku:
                 if self.array[ix][jx] == valor:
                     return False
         return True
+    def validaLargoMatriz(self):
+        if len(self.array) != self.N:
+            return False
+        for i in self.array:
+            if len(i) !=self.N:
+                return False
+        return True
+    def celdaObjetivo(self):
+        
+        for i in range(len(self.array)):
+            for j in range(len(self.array[0])):
+                if (self.array[i][j]==0):
+                    return i,j
+        return []
+        
+    def validaTodo(self,i,j,valor):
+        
+        return (self.validaFila(i,valor) and self.validaColumna(j,valor) and self.validaTresXTres(i,j,valor))
+        
         
     def resolver(self):
-        print self.array
+        if not self.validaLargoMatriz():
+            print "Matriz no respeta el formato 9x9"
+            return False
+        objetivo = self.celdaObjetivo()
+        if objetivo==[]:
+            return True
+        else:
+            i,j = objetivo
+        for valor in range(1,self.N+1):
+            if self.validaTodo(i,j,valor):
+                self.array[i][j] = valor
+                if(self.resolver()):
+                    return True
+                self.array[i][j]=0
+        return False
+                
+        
+
+        print "aqui empieza el mambo"
 array =[[3, 0, 6, 5, 0, 8, 4, 0, 0], 
           [5, 2, 0, 0, 0, 0, 0, 0, 0], 
           [0, 8, 7, 0, 0, 0, 0, 3, 1], 
@@ -38,6 +84,13 @@ array =[[3, 0, 6, 5, 0, 8, 4, 0, 0],
           [1, 3, 0, 0, 0, 0, 2, 5, 0], 
           [0, 0, 0, 0, 0, 0, 0, 7, 4], 
           [0, 0, 5, 2, 0, 6, 3, 0, 0]]
-print sudoku(array).validaTresXTres(8,8,6)
+
+array =[[5,1,7,6,0,0,0,3,4],[2,8,9,0,0,4,0,0,0],[3,4,6,2,0,5,0,9,0],[6,0,2,0,0,0,0,1,0],[0,3,8,0,0,6,0,4,7],[0,0,0,0,0,0,0,0,0],[0,9,0,0,0,0,0,7,8],[7,0,3,4,0,0,5,6,0],[0,0,0,0,0,0,0,0,0]]
+instancia = sudoku(array)
+if instancia.resolver():
+    print "Sudoku resuelto"
+    sudoku(array).imprimeMatriz()
+else:
+    print "No se puede resolver sudoku"
 
         
